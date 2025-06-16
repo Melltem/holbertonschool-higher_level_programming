6d9c5d5201f36c27fb6793583f3582a416ec21e5
Melltem <meltemqasimli@gmail.com>
2025-06-16 11:52:08 +0000

    gg

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,6 +3,6 @@ import sys
 count = len(sys.argv[1:])
 element = sys.argv[1:]
 if __name__ == "__main__":
-    print(count, "arguments:")
+    print(count, "argument:")
     for i,element in enumerate(element, start=1):
         print(f"{i}: { element}")
