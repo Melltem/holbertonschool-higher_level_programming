1522ca54e4a5b51653db3ab13c003641725f5e93
Melltem <meltemqasimli@gmail.com>
2025-06-16 11:59:28 +0000

    ff

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,8 +2,11 @@
 import sys
 count = len(sys.argv[1:])
 element = sys.argv[1:]
-word = "argument:" if count == 1 else "arguments:"
-if __name__ == "__main__":
-    print(count, word)
-    for i,element in enumerate(element, start=1):
-        print(f"{i}: { element}")
+if count == 0:
+    print("0 arguments.")
+else:
+    word = "argument:" if count == 1 else "arguments:"
+    if __name__ == "__main__":
+        print(count, word)
+        for i,element in enumerate(element, start=1):
+            print(f"{i}: { element}")
