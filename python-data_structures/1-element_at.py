14585f478ca778da19492e53a98a30a14c9019b5
Melltem <meltemqasimli@gmail.com>
2025-06-17 08:58:29 +0000

    fg

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
-    elif idx > my_list:
-        print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+    if idx < 0 or idx >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
