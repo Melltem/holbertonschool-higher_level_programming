0f8c773ebbdb265228c756e935169a25ab910254
Melltem <meltemqasimli@gmail.com>
2025-06-17 17:23:55 +0000

    sds

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+
+    best_key = ""
+    highest_value = 0
+
+    for key in a_dictionary:
+        if a_dictionary[key] > highest_value:
+            highest_value = a_dictionary[key]
+            best_key = key
+
+    return best_key
