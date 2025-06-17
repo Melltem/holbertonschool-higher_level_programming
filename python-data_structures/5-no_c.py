24ae05d87b2136b9bf7baa37965dd4ea92dcf163
Melltem <meltemqasimli@gmail.com>
2025-06-17 10:30:20 +0000

    sa

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = my_string.replace('c', '')
-    my_string = my_string.replace('C', '')
-    return my_string
-
+    result = ''
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            result += i
+    return result
