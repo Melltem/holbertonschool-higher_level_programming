144cde281db289c19077c7da7ebefebe18b645ea
Melltem <meltemqasimli@gmail.com>
2025-06-20 09:19:25 +0000

    sds

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    idx = 0
+    count = 0
     try:
-        for i in my_list:
-            if idx < x:
-                print('{}'.format(my_list[idx]), end='')
-                idx += 1
-        print()
-    except TypeError:
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
         pass
-    finally:
-        return idx
+    print()
+    return count
