48ec22c88e2216132c50c2bfa7c163f67c065fce
Melltem <meltemqasimli@gmail.com>
2025-06-27 14:38:42 +0000

    0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,4 @@
+def read_file(filename=""):
+    """Read a UTF-8 text file and print its contents to stdout."""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
