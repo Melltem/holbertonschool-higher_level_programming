044c3e02281015a37cdd0e4c3384e0ac80c827b3
Melltem <meltemqasimli@gmail.com>
2025-06-27 16:50:51 +0000

    11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -12,8 +12,10 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
-            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        if (isinstance(attrs, list) and
+                all(isinstance(attr, str) for attr in attrs)):
+            return {key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)}
         return self.__dict__
 
     def reload_from_json(self, json):
