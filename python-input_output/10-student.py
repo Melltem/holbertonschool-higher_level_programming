5f1bfccb4e94cec52fe6d7f1a5fd0766fa56e34b
Melltem <meltemqasimli@gmail.com>
2025-06-27 16:45:41 +0000

    10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
-"""Defines a class Student with attribute filtering in to_json."""
+"""Defines a class Student."""
 
 
 class Student:
     """Represents a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student.
-
-        Args:
-            first_name (str): The student's first name.
-            last_name (str): The student's last name.
-            age (int): The student's age.
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return a dictionary representation of the Student.
-
-        If attrs is a list of strings, return only those attributes.
-        Otherwise, return all attributes.
-        """
         if (type(attrs) == list and
                 all(type(attr) == str for attr in attrs)):
             return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
