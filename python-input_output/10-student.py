b29ca6561ba4c1e2f9b895cdebfd9aa09366281d
Melltem <meltemqasimli@gmail.com>
2025-06-27 16:41:05 +0000

    10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a class Student with
+attribute filtering in to_json."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (type(attrs) == list and
+                all(type(attr) == str for attr in attrs)):
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        return self.__dict__
