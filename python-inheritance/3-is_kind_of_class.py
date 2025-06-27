faf3c739c424f4784b6daf7d08d84c14cd859504
Melltem <meltemqasimli@gmail.com>
2025-06-27 13:19:24 +0000

    3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Module to check if an object is  a class."""
+"""Module to check if an object is a class."""
 
 
 def is_kind_of_class(obj, a_class):
-     """Return True if obj is an instance of a_class or its subclass,
-     else False."""
+    """Return True if obj is an instance of a_class or its subclass,
+    else False."""
     return isinstance(obj, a_class)
