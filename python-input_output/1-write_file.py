a843bd6395adbaf7068e8e79228b52837913bbcd
Melltem <meltemqasimli@gmail.com>
2025-06-27 14:41:31 +0000

    0

--- python-input_output/1-write_file.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def write_file(filename="", text=""):
-    """Write a string to a UTF-8 text file and return the number of characters written."""
-    with open(filename, "w", encoding="utf-8") as file:
-        return file.write(text)
