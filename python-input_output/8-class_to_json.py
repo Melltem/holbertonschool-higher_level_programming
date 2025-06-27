c96f9e82a362fea39fe5cf305c361d91b9a0c23f
Melltem <meltemqasimli@gmail.com>
2025-06-27 16:26:26 +0000

    8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Function that returns the dictionary description of an object for JSON serialization."""
+"""Function that returns the dictionary description
+of an object for JSON serialization."""
 
 
 def class_to_json(obj):
-    """Return the dictionary description for JSON serialization of an object."""
+    """Return the dictionary description for JSON
+    serialization of an object."""
     return obj.__dict__
