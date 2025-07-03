27ff22611ca3de30de455bc75c423e50d23fc72f
Melltem <meltemqasimli@gmail.com>
2025-07-03 16:49:35 +0000

    3. Serializing and Deserializing with XML

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -8,3 +8,20 @@ def serialize_to_xml(dictionary, filename):
             item.text = str(dictionary[key])
         tree = ET.ElementTree(root)
         tree.write(filename)
+
+        return True
+    except:
+        return False
+
+def deserialize_from_xml(filename):
+    try:
+        tree = ET.parse(filename)
+        root = tree.getroot()
+
+        result = {}
+        for item in root:
+            result[item.tag] = item.text
+
+        return result
+    except:
+        return None
