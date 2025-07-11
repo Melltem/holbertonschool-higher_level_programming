db0d1467d218a0c053fce9dfcb23ffa4ea99f8c1
Melltem <meltemqasimli@gmail.com>
2025-07-11 14:37:00 +0000

    0. Whats my status? #0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import urllib.request
 
 url = "https://intranet.hbtn.io/status"
-headers = {'User-Agent': 'Mozilla/5.0'}
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Connection': 'keep-alive'
+}
 
 req = urllib.request.Request(url, headers=headers)
 
