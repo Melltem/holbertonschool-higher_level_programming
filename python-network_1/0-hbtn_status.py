2cae9c9a63c93a969392e7e6c74ca3298ffae2f3
Melltem <meltemqasimli@gmail.com>
2025-07-11 14:31:22 +0000

    0. Whats my status? #0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,20 +1,12 @@
-#!/usr/bin/python3
-"""
-Module documented
-"""
-
+#!/usr/bin/env python3
 import urllib.request
 
 url = "https://intranet.hbtn.io/status"
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.5",
-}
+headers = {'User-Agent': 'Mozilla/5.0'}
 
 req = urllib.request.Request(url, headers=headers)
 
 with urllib.request.urlopen(req) as response:
-    body = response.read().decode("utf-8")
-    print("\t- " + body)
+    body = response.read().decode('utf-8')
+    print("Body response:")
+    print("\t- {}".format(body))
