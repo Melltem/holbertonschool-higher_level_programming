d877a5b3db888b4451985ffea6a2e26e88996805
Melltem <meltemqasimli@gmail.com>
2025-07-11 15:03:54 +0000

    4. Whats my status? #1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -6,6 +6,7 @@ Fetches https://intranet.hbtn.io/status and displays the body with formatting.
 import requests
 
 if __name__ == "__main__":
+    """Ensure code is executed when directly run"""
     url = "https://intranet.hbtn.io/status"
     response = requests.get(url)
     body = response.text
