b478b07cc035fd39962bbe534e88196c9e94987a
Melltem <meltemqasimli@gmail.com>
2025-07-11 15:05:22 +0000

    4. Whats my status? #1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
-Fetches https://intranet.hbtn.io/status and displays the body with formatting.
-"""
-
+Fetches https://intranet.hbtn.io/status and displays the body with formatting"""
 import requests
 
 if __name__ == "__main__":
     """Ensure code is executed when directly run"""
     url = "https://intranet.hbtn.io/status"
-    response = requests.get(url)
-    body = response.text
+    headers = {'cfclearance': 'true'}
+    response = requests.get(url, headers=headers)
     print("Body response:")
-    print(f"\t- {body}")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
