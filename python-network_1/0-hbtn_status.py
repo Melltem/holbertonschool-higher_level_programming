37f44bdbb583b2c667ddbd481e36ecd5153e8620
Melltem <meltemqasimli@gmail.com>
2025-07-11 14:29:07 +0000

    0. Whats my status? #0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """Module documented"""
+
 import urllib.request
+
 url = "https://intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response:
+req = urllib.request.Request(
+    url,
+    headers={"User-Agent": "Mozilla/5.0"}
+)
+
+with urllib.request.urlopen(req) as response:
     body = response.read().decode("utf-8")
     print("\t- " + body)
